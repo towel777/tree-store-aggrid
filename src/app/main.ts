dd3989ae29d38083ff24ba554182567c5a2dd041
NikitaDEV <NikitaDEV>
2026-01-14 16:24:02 +0300

    init ag-grid-community

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
 import App from './App.vue'
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 const app = createApp(App)
 
